7dda96f25329885bf163323570db3e51b47d63ee
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2025-10-31 19:55:34 +0600

    fix(gdpr-statement): rename default export function for clarity

--- src/app/(root)/gdpr-statement/page.tsx
+++ src/app/(root)/gdpr-statement/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from "@/components/contact-form/ContactForm";
 import Complaint from "@/components/gdrp-statement/complaint";
 import HeroSection from "@/components/gdrp-statement/HeroSection";
 
-export default function () {
+export default function GDPRStatementPage() {
   return (
     <>
       <HeroSection />
