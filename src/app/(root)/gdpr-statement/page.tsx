932c0f0b2c24013611f9d298a4d9e760420af960
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2025-10-31 19:52:27 +0600

    feat(gdpr-statement): add GDPR Statement page with HeroSection, Complaint, and ContactForm components

---
+++ src/app/(root)/gdpr-statement/page.tsx
@@ -0,0 +1,13 @@
+import ContactForm from "@/components/contact-form/ContactForm";
+import Complaint from "@/components/gdrp-statement/complaint";
+import HeroSection from "@/components/gdrp-statement/HeroSection";
+
+export default function () {
+  return (
+    <>
+      <HeroSection />
+      <Complaint />
+      <ContactForm />
+    </>
+  );
+}
---
+++ src/components/gdrp-statement/complaint.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+
+const Complaint = () => {
+  return (
+    <section className="py-16 bg-white">
+      <div className="container mx-auto px-6 lg:px-12">
+        <h3 className="text-xl font-bold mb-4 leading-tight text-primary">
+          We Are Globex UK GDPR Compliance Statement
+        </h3>
+
+        <p className="text-lg font-bold mb-4 leading-tight text-gray-900">
+          The UK General Data Protection Regulation (UK GDPR), along with the
+          Data Protection Act 2018 (DPA 2018), governs the protection and
+          privacy of personal data within the United Kingdom. Following the UK’s
+          departure from the European Union, UK GDPR was introduced to retain
+          the core principles of the EU GDPR while adapting certain provisions
+          for UK-specific data protection laws.
+        </p>
+
+        <h3 className="text-xl font-bold mb-4 leading-tight text-primary">
+          Scope of UK GDPR Application
+        </h3>
+
+        <p className="text-lg font-bold mb-20 leading-tight text-gray-900">
+          UK GDPR applies to data processing activities conducted by
+          organizations operating within the UK and to organizations outside the
+          UK that offer goods or services to individuals in the UK. We Are
+          Globex is committed to complying with the UK GDPR as both a data
+          controller and processor.
+        </p>
+
+        <h3 className="text-xl font-bold mb-4 leading-tight text-primary">
+          Our Commitment to UK GDPR Compliance
+        </h3>
+
+        <p className="text-lg font-bold mb-4 leading-tight text-gray-900">
+          We Are Globex is dedicated to maintaining the highest standards of
+          data protection in line with the UK GDPR and DPA 2018. We have
+          implemented internal policies to ensure compliance, with staff trained
+          on these procedures. Our data protection policies are regularly
+          reviewed and updated to stay in compliance with UK-specific
+          requirements.
+        </p>
+
+        <h3 className="text-xl font-bold mb-4 leading-tight text-gray-900">
+          Data Handling and Protection
+        </h3>
+
+        <p className="text-lg font-bold mb-4 leading-tight text-gray-900">
+          We only collect and process data when it is necessary to provide
+          services, comply with legal obligations, or facilitate transactions.
+          We ensure that all personal data is treated with integrity and
+          confidentiality, whether stored electronically or offline, and
+          retained only as long as required. Data is securely deleted once it is
+          no longer needed.
+        </p>
+
+        <h3 className="text-xl font-bold mb-4 leading-tight text-gray-900">
+          Third-Party Compliance
+        </h3>
+
+        <p className="text-lg font-bold mb-4 leading-tight text-gray-900">
+          We Are Globex takes all reasonable steps to ensure that third-party
+          suppliers and partners who process data on our behalf comply with UK
+          GDPR. We require that they adhere to the same high standards of data
+          protection as we do.
+        </p>
+
+        <h3 className="text-xl font-bold mb-4 leading-tight text-gray-900">
+          Technology and Security Measures
+        </h3>
+
+        <p className="text-lg font-bold mb-10 leading-tight text-gray-900">
+          Our technology platforms are regularly tested and updated to ensure
+          that they meet the highest security standards, in line with UK GDPR
+          and DPA 2018. We employ robust security measures to protect personal
+          data from unauthorized access, loss, or misuse.
+        </p>
+
+        <hr />
+
+        <h3 className="text-xl font-bold mb-4 leading-tight text-gray-900 mt-6">
+          Contact Us
+        </h3>
+
+        <p className="text-lg font-bold mb-5 leading-tight text-gray-900">
+          For any questions regarding how we handle personal data or our
+          compliance with UK GDPR, please contact us:
+        </p>
+
+        <p className="text-lg font-bold mb-16 leading-tight text-gray-900">
+          <div className="space-y-2">
+            • <strong>By email:</strong> info@weareglobex.com
+          </div>
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Complaint;
