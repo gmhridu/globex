77e2afaf6758142e7473ab7cb3b2182aeb95adc9
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2025-10-31 19:40:56 +0600

    Add Privacy Policy page with HeroSection, Policies, and ContactForm components

---
+++ src/app/(root)/privacy-policy/page.tsx
@@ -0,0 +1,13 @@
+import ContactForm from "@/components/contact-form/ContactForm";
+import HeroSection from "@/components/privacy-policy/HomeSection";
+import Policies from "@/components/privacy-policy/Policies";
+
+export default function PrivayPolicyPage() {
+  return (
+    <>
+      <HeroSection />
+      <Policies />
+      <ContactForm />
+    </>
+  );
+}
---
+++ src/components/privacy-policy/HomeSection.tsx
@@ -0,0 +1,25 @@
+const HeroSection = () => {
+  return (
+    <section className="relative bg-secondary pt-24 min-h-[60vh] flex items-center">
+      {/* Background Image with Overlay */}
+      <div
+        className="absolute inset-0 bg-cover bg-center z-0 hero-background2"
+        style={{
+          backgroundImage: "url('/assests/privacy-policy/heroImage.jpg')",
+          backgroundBlendMode: "overlay",
+        }}
+      ></div>
+
+      {/* Content */}
+      <div className="container relative z-10 py-16 md:py-24">
+        <div className="max-w-[1300px]">
+          <h1 className="text-primary text-4xl md:text-5xl text-center">
+            Privacy Policy
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
